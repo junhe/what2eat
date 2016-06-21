08e9171f93a8ec898737262c4c587f8f9c383c15
Jun He <jhe@cs.wisc.edu>
2016-06-20 21:51:56 -0500

    Add many tests

---
+++ tests/test.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from what2eat.utils import *
+from what2eat.table import *
+
+class TestReadingXLS(unittest.TestCase):
+    def test_simple(self):
+        table = read_xls("./tests/sample.xlsx")
+
+        self.assertEqual(len(table), 2)
+        self.assertEqual(table[0]['col1'], 1)
+        self.assertEqual(table[1]['col1'], 2)
+        self.assertEqual(table[0]['col2'], 'a')
+        self.assertEqual(table[1]['col2'], 'b')
+
+    def test_chinese(self):
+        table = read_xls("./tests/menusample.xlsx")
+        row0 = table[0]
+        self.assertEqual(row0['EntryName'], u"皮蛋瘦肉粥")
+
+
+def create_test_table():
+    origin = [{'col1': 1, 'col2': 2},
+             {'col1': 3, 'col2': 4}]
+    table = Table(origin)
+    return table
+
+
+class TestTable(unittest.TestCase):
+    def test_init(self):
+        origin = [{'col1': 1, 'col2': 2},
+                 {'col1': 3, 'col2': 4}]
+        table = Table(origin)
+        self.assertDictEqual({'col1': 1, 'col2': 2},
+                table.get_row(0))
+        self.assertDictEqual({'col1': 3, 'col2': 4},
+                table.get_row(1))
+
+    def test_filter(self):
+        table = create_test_table()
+        rows = table.filter_equal('col1', 3)
+        self.assertListEqual(rows, [{'col1': 3, 'col2': 4}])
+
+    def test_load_excel(self):
+        table_excel = read_xls("./tests/sample.xlsx")
+        local_table = Table(table_excel)
+        rows = local_table.rows()
+
+        self.assertEqual(len(rows), 2)
+        self.assertEqual(rows[0]['col1'], 1)
+        self.assertEqual(rows[1]['col1'], 2)
+        self.assertEqual(rows[0]['col2'], 'a')
+        self.assertEqual(rows[1]['col2'], 'b')
+
+
+class TestChineseTable(unittest.TestCase):
+    def test_filter(self):
+        table_excel = read_xls("./tests/menusample.xlsx")
+        table = Table(table_excel)
+
+        rows = table.filter_equal('AddEntry', 1)
+        self.assertEqual(rows[0]['EntryName'], u"肉末酸豆角")
+
+        rows = table.filter_equal('AddEntry', None)
+        self.assertEqual(rows[0]['EntryName'], u"皮蛋瘦肉粥")
+
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
+
--- what2eat/table.py
+++ what2eat/table.py
@@ -1,13 +1,26 @@
 from copy import deepcopy
 
+"""
+- Read Excel file
+- Generate a table with contents of the excel file
+- Pick n meat items
+- Pick m vege items
+- Send the ingredients to TODOist
+- Save generated menu as Numbers file
+"""
+
 class Table(object):
     def __init__(self, table):
-        self._table = deepcopy(table)
+        self._rows = deepcopy(table)
 
     def filter_equal(self, colname, value):
-        result = [row for row in self._table if row[colname] == value]
-        return deepcopy(result)
+        result = [row for row in self._rows if row[colname] == value]
+        return result
 
     def get_row(self, rowid):
-        return self._table[rowid]
+        return self._rows[rowid]
+
+    def rows(self):
+        return self._rows
+
 
